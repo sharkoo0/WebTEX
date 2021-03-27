856cc4bdc5336ca84b2b2c19a9a362c139425b32
Daniel Radev <sharkoo0@abv.bg>
2021-03-27 19:57:43 +0200

    Fixed errors from the comments

--- changePassword.js
+++ changePassword.js
@@ -1,27 +1,13 @@
 function next() {
-    let firstField = document.getElementsByClassName('pass');
-    let secondField = document.getElementsByClassName('conf-pass');
-    let button = document.getElementById('next');
-    let finish = document.getElementById('finish');
-
-    //TODO fix the if statements
-    // if(firstField[0].innerText.length === 0 || secondField[0].innerText.length === 0) {
-    //     alert("The fields above are required!");
-    // }
-    // if(firstField[0].innerText != secondField[0].innerText) {
-    //     alert("The passwords don't match!"); 
-    // }
-    
+    const firstField = document.getElementsByClassName('pass');
+    const secondField = document.getElementsByClassName('conf-pass');
+    const button = document.getElementById('next');
+    const finish = document.getElementById('finish');
+    const correctColor = "#66cc66";
+    const incorrectColor = "#ff6666";
 
     firstField[0].placeholder = 'Enter new password*';
     secondField[0].placeholder = 'Re-enter new password*';
-
-    //TODO add checkbox before the button
-
-    // let checkbox = document.createElement('input');
-    // checkbox.setAttribute('type', 'checkbox');
-
-    //button.innerText = 'Finish';
     button.style.visibility = 'hidden';
     finish.style.visibility = 'visible';
 }
@@ -33,3 +19,7 @@ function finish() {
     alert("Your password is updated.");
 }
 
+function back() {
+    const url = 'myuser.html';
+    window.location = url;
+}
