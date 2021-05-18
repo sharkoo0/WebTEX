fab6f3b08b2257e383b7830fce21582472f00c7f
adikrasimirowa <adikrasimirowa@abv.bg>
2021-05-19 00:39:43 +0300

    frontend fixes

--- client/js/nav.js
+++ client/js/nav.js
@@ -9,7 +9,12 @@ function changeStyle() {
 
 function upload() {
     const modal = document.getElementsByClassName('wrapper modal');
-    console.log(modal[0]);
+    // console.log(modal[0]);
 
     modal[0].style.display = 'block';
+}
+
+function cancelUpdate() {
+    const modal = document.getElementsByClassName('wrapper modal');
+    modal[0].style.display = 'none';
 }
