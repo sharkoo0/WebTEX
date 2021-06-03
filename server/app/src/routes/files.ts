3af68058c2b447848adcd0bb84fd2b94bf20a515
AleksandrinaKovachka <aleksandrina.kowachka@gmail.com>
2021-06-03 23:41:11 +0300

    Add get request for search file in db

--- server/app/src/routes/files.ts
+++ server/app/src/routes/files.ts
@@ -5,6 +5,7 @@ import fileService from '../services/fileService';
 import  FileSchema from '../schemas/fileSchema';
 import { Upload, uploadFiles, deleteFiles } from '../controllers/fileController';
 import UserSchema from '../schemas/userSchema';
+import mongoose from 'mongoose'
 
 const filesRouter = Express.Router();
 
@@ -12,16 +13,24 @@ filesRouter.post('/upload', Upload.array('file-to-upload'), uploadFiles);
 filesRouter.delete('/delete', deleteFiles);
 
 filesRouter.get('/get', async (req, res) => {
-  // FileSchema.find().then(file => res.json(file));
-  const filename = req.body.filename;
-  const user = await UserSchema.findOne({username: req.body.username}, (err: Error, user: any) => {
+  const filename = req.body.fileName;
+  const user = await UserSchema.findOne({username: req.body.username}, async (err: Error, user: any) => {
     if(err) {
       res.status(400).json({"error": err});
     }
     const file = user.files;
+    const db = mongoose.connection.db.collection('users');
+    const userFromDB = await db.findOne({username: req.body.username});
+    const files: Array<any> = userFromDB.files;
+    let result: Array<any> = [];
+    files.forEach(l => {
+      if(l.name === filename){
+        res.status(200).json(l);
+      }
+    }) 
+    res.status(400).json({"error: No such a file": ""});
   })
-  console.log(filename);
-  res.json(filename)
+  
 });
 
 export { filesRouter };
