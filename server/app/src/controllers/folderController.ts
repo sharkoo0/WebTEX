df302f8ccd4567200d88339fc8f3aeda6c30a933
adikrasimirowa <adikrasimirowa@abv.bg>
2021-06-03 21:34:28 +0300

    more changes

--- server/app/src/controllers/folderController.ts
+++ server/app/src/controllers/folderController.ts
@@ -1,21 +1,20 @@
 import Express from 'express';
 import { Folder } from '../models/folderModel';
-import Multer from 'multer';
-import fs from 'fs';
 import FolderSchema from '../schemas/folderSchema';
 import FolderService from '../services/folderService';
 
 const postMethod = async (req: Express.Request, res: Express.Response) => {
-  const body: Folder = req.body;
+  const newFolder: Folder = req.body;
   console.log(req.body);
-  if (body) {
-    // await FolderService.change(body.name).then(() => {
-    //   res.sendStatus(200);
-    // }).catch(error => {
-    //   // res.redirect('/');
-    //   res.sendStatus(401);
-    //   console.log(error);
-    // })
+  if (newFolder) {
+    await FolderService.addFolder(newFolder.name).then(() => {
+      console.log(newFolder.name);
+      res.sendStatus(200);
+    }).catch(error => {
+      // res.redirect('/');
+      res.sendStatus(401);
+      console.log(error);
+    })
   }
 };
 
